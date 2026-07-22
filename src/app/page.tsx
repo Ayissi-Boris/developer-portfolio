64280c601d38f6ea468cdeba8a8d998a19302d19
Boris-Ayissix <borisayissix@gmail.com>
2026-07-22 01:47:08 +0100

    feat: add footer section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@ import Hero from "@/src/components/Hero";
 import Projects from "@/src/components/Projects";
 import Navbar from "@/src/components/Navbar";
 import Footer from "@/src/components/Footer";
+import About from "@/src/components/About";
 
 export default function Home() {
   return (
     <main>
       <Navbar />
       <Hero />
+      <About />
       <Projects />
       <Footer />
 
